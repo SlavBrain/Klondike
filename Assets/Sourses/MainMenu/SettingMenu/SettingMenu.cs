e3fcad52f15244b3df912d6f70c72b59a2435cbf
Egor Borisov <82366112+SlavBrain@users.noreply.github.com>
2023-09-27 03:54:07 +0300

    fix saving music mute

--- Assets/Sourses/MainMenu/SettingMenu/SettingMenu.cs
+++ Assets/Sourses/MainMenu/SettingMenu/SettingMenu.cs
@@ -3,11 +3,13 @@ using UnityEngine.UI;
 
 public class SettingMenu : MonoBehaviour
 {
-    [SerializeField] private SoundMuteChanger _muteChanger;
+    [SerializeField] private SoundMuteChanger _soundMuteChanger;
+    [SerializeField] private MusicMuteChanger _musicMuteChanger;
 
     public void Initialize()
     {
-        _muteChanger.Initialize();
+        _soundMuteChanger.Initialize();
+        _musicMuteChanger.Initialize();
     }
     
     public void Enable() => gameObject.SetActive(true);
